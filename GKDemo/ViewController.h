247c5ad7d8a37b2bf7c81e15110282805ea37a51
Gabriel Theodoropoulos <gabriel@Gabriel-Theodoropouloss-Mac-mini.local>
2014-02-10 19:25:20 +0200

    Initial Commit

---
+++ GKDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GKDemo
+//
+//  Created by Gabriel Theodoropoulos on 10/2/14.
+//  Copyright (c) 2014 AppCoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GKDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GKDemo
+//
+//  Created by Gabriel Theodoropoulos on 10/2/14.
+//  Copyright (c) 2014 AppCoda. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
